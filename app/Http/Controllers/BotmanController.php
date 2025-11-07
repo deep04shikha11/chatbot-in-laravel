16fa6296bf9a27c015f1e934a181bc5983db911d
deep04shikha11 <deepshikhaverma44@gmail.com>
2025-11-07 18:12:26 +0530

    correction in bot

--- app/Http/Controllers/BotmanController.php
+++ app/Http/Controllers/BotmanController.php
@@ -3,36 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use botman\botman\BotMan;
-use botman\botman\Messages\Incoming\Answer;
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Incoming\Answer;
 
-class BotmanController extends Controller
-{
-    public function chatbot(){
+class BotmanController extends Controller {
+    public function chatbot() {
         $botman = app('botman');
-  
-        $botman->hears('{message}', function($botman, $message) {
-  
+
+        $botman->hears('{message}', function ($botman, $message) {
+
             if ($message == 'hi') {
                 $this->askName($botman);
-            }else{
+            } else {
                 $botman->reply("write 'hi' for testing...");
             }
-  
         });
-  
+
         $botman->listen();
     }
 
-    public function askName($botman)
-    {
-        $botman->ask('Hello! First, tell me your First and Last name ?', function(Answer $answer) {
-  
+    public function askName($botman) {
+        $botman->ask('Hello! First, tell me your First and Last name ?', function (Answer $answer) use ($botman) {
+
             $name = $answer->getText();
-  
-            $this->say('Nice to meet you '.$name);
+
+            $botman->reply('Nice to meet you ' . $name);
         });
     }
-
-
 }
